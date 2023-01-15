37bafb99344d69ca87c0a5e207ab9da7fc86d061
Matt Butterfield <matt@mattbutterfield.com>
2023-01-14 21:20:51 -0500

    no follow button on own page

--- app/static/ts/app/user_home/index.tsx
+++ app/static/ts/app/user_home/index.tsx
@@ -2,15 +2,17 @@ import {useQuery} from "@apollo/client";
 import Alert from "@mui/material/Alert";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
+import {AppContext} from "app/index";
 import PostItem from "app/lib/components/PostItem";
 import {FollowButton} from "app/user_home/FollowButton";
 import {GET_USER_POSTS} from "app/user_home/queries";
 import {Post} from "graphql/types";
-import React from "react";
+import React, {useContext} from "react";
 import {useParams} from "react-router-dom";
 
 
 const UserHome = () => {
+  const {user} = useContext(AppContext);
   const {username} = useParams();
   const {data, loading, error} = useQuery(GET_USER_POSTS, {variables: {username: username}});
 
@@ -23,9 +25,11 @@ const UserHome = () => {
 
   return <Stack direction="column" alignItems="center" spacing={2} width={800} m="auto">
     <Typography variant="h2">{username}</Typography>
-    <FollowButton
-      homeUser={data.getUserPosts.user}
-    />
+    {user.id !== data.getUserPosts.user.id &&
+      <FollowButton
+        homeUser={data.getUserPosts.user}
+      />
+    }
     {data.getUserPosts.posts.map((post: Post) => {
       return <PostItem key={post.id} post={post} />;
     })}
