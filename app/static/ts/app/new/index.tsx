bfe0dec612d526b7311cbeeac4ce06fa5ee67a6c
Matt Butterfield <matt@mattbutterfield.com>
2023-01-22 17:41:32 -0500

    useeffects

--- app/static/ts/app/new/index.tsx
+++ app/static/ts/app/new/index.tsx
@@ -1,11 +1,11 @@
 import {gql, useQuery} from "@apollo/client";
 import Stack from "@mui/material/Stack";
-import {debounce} from "@mui/material/utils";
+import Typography from "@mui/material/Typography";
+import {setRef} from "@mui/material/utils";
 import {AppContext} from "app";
 import PostItem from "app/lib/components/PostItem";
 import {Post} from "graphql/types";
-import React, {useContext, useEffect, useState} from "react";
-import Typography from "@mui/material/Typography";
+import React, {useCallback, useContext, useEffect, useRef, useState} from "react";
 
 const GET_NEW_POSTS = gql`
   query getNewPosts($before: Time) {
@@ -31,19 +31,34 @@ const New = () => {
   const {data, loading, error, refetch} = useQuery(GET_NEW_POSTS);
   const message = loading ? "Loading..." : error ? "Error loading posts..." : "";
   const [posts, setPosts] = useState<Post[]>([]);
+  const [noNewData, setNoNewData] = useState(false);
+  const [refetched, setRefetched] = useState(false);
 
   useEffect(() => {
     if (data?.getNewPosts) {
-      setPosts([...posts, ...data.getNewPosts]);
-
-      window.addEventListener("scroll", debounce(async () => {
-        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 50) {
-          const before = data.getNewPosts[data.getNewPosts.length - 1].publishedAt;
-          await refetch({before: before});
-        }
-      }, 250));
+      if (!data.getNewPosts.length) {
+        setNoNewData(true);
+      } else {
+        setPosts([...posts, ...data.getNewPosts]);
+      }
     }
   }, [data]);
+
+  useEffect(() => {
+    if (noNewData) {
+      return;
+    }
+    const refetchEvent = async () => {
+      if (!refetched && window.innerHeight + window.scrollY >= document.body.offsetHeight - 50) {
+        setRefetched(true);
+        const before = posts[posts.length - 1].publishedAt;
+        await refetch({before: before});
+        setRefetched(false);
+      }
+    };
+    window.addEventListener("scroll", refetchEvent);
+    return () => window.removeEventListener("scroll", refetchEvent);
+  }, [noNewData, posts]);
 
   return <Stack direction="column" alignItems="center" spacing={2} width={800} m="auto">
     <Typography variant="h2">Welcome{user && `, ${user.username}.`}</Typography>
