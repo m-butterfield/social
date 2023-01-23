aeaffb05a1959ec7eb6444d072836f6a1a15b2ca
Matt Butterfield <matt@mattbutterfield.com>
2023-01-22 20:46:01 -0500

    shared scrollable component

---
+++ app/static/ts/app/lib/components/ScrollablePosts/index.tsx
@@ -0,0 +1,72 @@
+import {NetworkStatus, TypedDocumentNode, useQuery} from "@apollo/client";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import {AppContext} from "app";
+import PostItem from "app/lib/components/PostItem";
+import {Post} from "graphql/types";
+import React, {useContext, useEffect, useState} from "react";
+
+type ScrollablePostsProps = {
+  query: TypedDocumentNode;
+  queryName: string;
+}
+
+const ScrollablePosts = ({
+  query,
+  queryName,
+}: ScrollablePostsProps) => {
+  const {user} = useContext(AppContext);
+
+  const {data, loading, error, refetch, networkStatus} = useQuery(
+    query, {notifyOnNetworkStatusChange: true}
+  );
+  const refetching = networkStatus === NetworkStatus.setVariables; // this should be NetworkStatus.refetch but there is a bug: https://github.com/apollographql/apollo-client/issues/10391
+  const message = loading && !refetching ? "Loading..." : error ? "Error loading posts..." : "";
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [noNewData, setNoNewData] = useState(false);
+  const [before, setBefore] = useState("");
+
+  useEffect(() => {
+    if (data && data[queryName]) {
+      if (!data[queryName].length) {
+        setNoNewData(true);
+      } else {
+        setPosts([...posts, ...data[queryName]]);
+      }
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (before) {
+      refetch({before: before});
+    }
+  }, [before]);
+
+  useEffect(() => {
+    if (noNewData) {
+      return;
+    }
+    const refetchEvent = async () => {
+      if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 200) {
+        setBefore(posts[posts.length - 1].publishedAt);
+      }
+    };
+    window.addEventListener("scroll", refetchEvent);
+    return () => window.removeEventListener("scroll", refetchEvent);
+  }, [noNewData, posts]);
+
+  return <Stack direction="column" alignItems="center" spacing={2} width={800} m="auto">
+    <Typography variant="h2">Welcome{user && `, ${user.username}.`}</Typography>
+    {message ?
+      <Typography>{message}</Typography>
+      :
+      posts.map((post: Post) => {
+        return <PostItem key={post.id} post={post} />;
+      })
+    }
+    {refetching && <Typography>Loading more posts...</Typography>}
+    {noNewData && <Typography>No more posts.</Typography>}
+  </Stack>;
+};
+
+export default ScrollablePosts;
--- app/static/ts/app/new/index.tsx
+++ app/static/ts/app/new/index.tsx
@@ -1,10 +1,6 @@
-import {gql, NetworkStatus, useQuery} from "@apollo/client";
-import Stack from "@mui/material/Stack";
-import Typography from "@mui/material/Typography";
-import {AppContext} from "app";
-import PostItem from "app/lib/components/PostItem";
-import {Post} from "graphql/types";
-import React, {useContext, useEffect, useState} from "react";
+import {gql} from "@apollo/client";
+import ScrollablePosts from "app/lib/components/ScrollablePosts";
+import React from "react";
 
 const GET_NEW_POSTS = gql`
   query getNewPosts($before: Time) {
@@ -25,58 +21,10 @@ const GET_NEW_POSTS = gql`
 `;
 
 const New = () => {
-  const {user} = useContext(AppContext);
-
-  const {data, loading, error, refetch, networkStatus} = useQuery(
-    GET_NEW_POSTS, {notifyOnNetworkStatusChange: true}
-  );
-  const refetching = networkStatus === NetworkStatus.setVariables; // this should be NetworkStatus.refetch but there is a bug: https://github.com/apollographql/apollo-client/issues/10391
-  const message = loading && !refetching ? "Loading..." : error ? "Error loading posts..." : "";
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [noNewData, setNoNewData] = useState(false);
-  const [before, setBefore] = useState("");
-
-  useEffect(() => {
-    if (data?.getNewPosts) {
-      if (!data.getNewPosts.length) {
-        setNoNewData(true);
-      } else {
-        setPosts([...posts, ...data.getNewPosts]);
-      }
-    }
-  }, [data]);
-
-  useEffect(() => {
-    if (before) {
-      refetch({before: before});
-    }
-  }, [before]);
-
-  useEffect(() => {
-    if (noNewData) {
-      return;
-    }
-    const refetchEvent = async () => {
-      if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 200) {
-        setBefore(posts[posts.length - 1].publishedAt);
-      }
-    };
-    window.addEventListener("scroll", refetchEvent);
-    return () => window.removeEventListener("scroll", refetchEvent);
-  }, [noNewData, posts]);
-
-  return <Stack direction="column" alignItems="center" spacing={2} width={800} m="auto">
-    <Typography variant="h2">Welcome{user && `, ${user.username}.`}</Typography>
-    {message ?
-      <Typography>{message}</Typography>
-      :
-      posts.map((post: Post) => {
-        return <PostItem key={post.id} post={post} />;
-      })
-    }
-    {refetching && <Typography>Loading more posts...</Typography>}
-    {noNewData && <Typography>No more posts.</Typography>}
-  </Stack>;
+  return <ScrollablePosts
+    query={GET_NEW_POSTS}
+    queryName={"getNewPosts"}
+  />;
 };
 
 export default New;
